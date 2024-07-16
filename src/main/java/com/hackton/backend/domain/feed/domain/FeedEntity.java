8ab7f8067cde8ba75c6cdc4c105b453116d11257
이정호 <dlwjdgh1115@gmail.com>
2024-07-16 15:42:30 +0900

    add: feed entity

--- src/main/java/com/hackton/backend/domain/feed/domain/FeedEntity.java
+++ src/main/java/com/hackton/backend/domain/feed/domain/FeedEntity.java
@@ -1,4 +1,41 @@
 package com.hackton.backend.domain.feed.domain;
 
+import com.hackton.backend.domain.user.domain.UserEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class FeedEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(30)", nullable = false)
+    private String accountId;
+
+    @Column(columnDefinition = "VARCHAR(3000)", nullable = false)
+    private String password;
+
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    @Column(columnDefinition = "BIGINT DEFAULT 0", nullable = false)
+    private Long likeCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private UserEntity user;
 }
