75c0396aff9aaea404b0c55f2d15488956b86626
gwonjiseong65@gmail.com <163107918+gwon08@users.noreply.github.com>
2024-07-17 08:56:49 +0900

    add: create feed

--- src/main/java/com/hackton/backend/domain/feed/presentation/FeedController.java
+++ src/main/java/com/hackton/backend/domain/feed/presentation/FeedController.java
@@ -1,13 +1,10 @@
 package com.hackton.backend.domain.feed.presentation;
 
+import com.hackton.backend.domain.feed.presentation.dto.request.CreateFeedRequest;
 import com.hackton.backend.domain.feed.presentation.dto.response.FeedDetailResponse;
 import com.hackton.backend.domain.feed.service.FeedService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/feeds")
@@ -22,5 +19,23 @@ public class FeedController {
             @RequestHeader("X-identifier") String identifier
     ) {
         return feedService.getFeedDetailById(feedId, identifier);
+    }
+
+    @PostMapping
+    public void createFeed(
+            @RequestBody CreateFeedRequest request,
+            @RequestHeader("X-identifier") String identifier
+    ) {
+        feedService.createFeed(request, identifier);
+    }
+
+    @DeleteMapping
+    public void deleteFeed() {
+
+    }
+
+    @PatchMapping
+    public void modifyFeed() {
+
     }
 }
--- src/main/java/com/hackton/backend/domain/feed/service/FeedService.java
+++ src/main/java/com/hackton/backend/domain/feed/service/FeedService.java
@@ -2,6 +2,7 @@ package com.hackton.backend.domain.feed.service;
 
 import com.hackton.backend.domain.feed.domain.FeedEntity;
 import com.hackton.backend.domain.feed.domain.FeedRepository;
+import com.hackton.backend.domain.feed.presentation.dto.request.CreateFeedRequest;
 import com.hackton.backend.domain.feed.presentation.dto.response.FeedDetailResponse;
 import com.hackton.backend.domain.user.domain.UserEntity;
 import com.hackton.backend.domain.user.domain.UserRepository;
@@ -30,5 +31,27 @@ public class FeedService {
                 .likeCount(feed.getLikeCount())
                 .isMine(feed.getUser().getAccountId().equals(user.getAccountId()))
                 .build();
+
+        public void createFeed (CreateFeedRequest request, String userIdentifier){
+            UserEntity user = userRepository.findByIdentifier(userIdentifier)
+                    .orElseThrow(() -> new RuntimeException(""));
+
+            feedRepository.save(
+                    FeedEntity.builder()
+                            .title(request.getTitle())
+                            .content(request.getContent())
+                            .likeCount(0)
+                            .createDate(LocalDateTime.now())
+                            .status("")
+                            .user(user)
+                            .build()
+            );
+        }
+        public void deleteFeed () {
+
+        }
+        public void modifyFeed () {
+
+>>>>>>>Stashed changes
+        }
     }
-}
